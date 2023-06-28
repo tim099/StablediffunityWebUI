d47487c1939bfb370d47bfaa76e72eca71d16cca
Tim <tim11251994@gmail.com>
2023-06-28 23:02:17 +0800

    Update sdu_sampling

--- scripts/sdu_sampling.py
+++ scripts/sdu_sampling.py
@@ -212,59 +212,78 @@ def sample_heun(model, x, sigmas, extra_args=None, callback=None, disable=None, 
 @torch.no_grad()
 def sample_dpm_2(model, x, sigmas, extra_args=None, callback=None, disable=None, s_churn=0., s_tmin=0., s_tmax=float('inf'), s_noise=1.):
     """A sampler inspired by DPM-Solver-2 and Algorithm 2 from Karras et al. (2022)."""
+    sample_data = sample_start(model, inspect.currentframe().f_code.co_name, x, sigmas, extra_args, callback, disable)#SDU Hijack
+
     extra_args = {} if extra_args is None else extra_args
-    s_in = x.new_ones([x.shape[0]])
+    s_in = sample_data.x.new_ones([sample_data.x.shape[0]])
     for i in trange(len(sigmas) - 1, disable=disable):
+        #SDU Hijack Start
+        sample_data.step = i
+        global_setting.trigger(sample_data)
+        if(global_setting.skip_sample(sample_data)):
+            continue
+        #SDU Hijack End
+
         gamma = min(s_churn / (len(sigmas) - 1), 2 ** 0.5 - 1) if s_tmin <= sigmas[i] <= s_tmax else 0.
-        eps = torch.randn_like(x) * s_noise
+        eps = torch.randn_like(sample_data.x) * s_noise
         sigma_hat = sigmas[i] * (gamma + 1)
         if gamma > 0:
-            x = x + eps * (sigma_hat ** 2 - sigmas[i] ** 2) ** 0.5
-        denoised = model(x, sigma_hat * s_in, **extra_args)
-        d = to_d(x, sigma_hat, denoised)
+            sample_data.x = sample_data.x + eps * (sigma_hat ** 2 - sigmas[i] ** 2) ** 0.5
+        denoised = model(sample_data.x, sigma_hat * s_in, **extra_args)
+        d = to_d(sample_data.x, sigma_hat, denoised)
         if callback is not None:
-            callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigma_hat, 'denoised': denoised})
+            callback({'x': sample_data.x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigma_hat, 'denoised': denoised})
         if sigmas[i + 1] == 0:
             # Euler method
             dt = sigmas[i + 1] - sigma_hat
-            x = x + d * dt
+            sample_data.x = sample_data.x + d * dt
         else:
             # DPM-Solver-2
             sigma_mid = sigma_hat.log().lerp(sigmas[i + 1].log(), 0.5).exp()
             dt_1 = sigma_mid - sigma_hat
             dt_2 = sigmas[i + 1] - sigma_hat
-            x_2 = x + d * dt_1
+            x_2 = sample_data.x + d * dt_1
             denoised_2 = model(x_2, sigma_mid * s_in, **extra_args)
             d_2 = to_d(x_2, sigma_mid, denoised_2)
-            x = x + d_2 * dt_2
-    return x
+            sample_data.x = sample_data.x + d_2 * dt_2
+    sample_end(sample_data)#SDU Hijack
+    return sample_data.x
 
 
 @torch.no_grad()
 def sample_dpm_2_ancestral(model, x, sigmas, extra_args=None, callback=None, disable=None, eta=1., s_noise=1., noise_sampler=None):
     """Ancestral sampling with DPM-Solver second-order steps."""
+    sample_data = sample_start(model, inspect.currentframe().f_code.co_name, x, sigmas, extra_args, callback, disable)#SDU Hijack
     extra_args = {} if extra_args is None else extra_args
-    noise_sampler = default_noise_sampler(x) if noise_sampler is None else noise_sampler
-    s_in = x.new_ones([x.shape[0]])
+    noise_sampler = default_noise_sampler(sample_data.x) if noise_sampler is None else noise_sampler
+    s_in = sample_data.x.new_ones([sample_data.x.shape[0]])
     for i in trange(len(sigmas) - 1, disable=disable):
-        denoised = model(x, sigmas[i] * s_in, **extra_args)
+        #SDU Hijack Start
+        sample_data.step = i
+        global_setting.trigger(sample_data)
+        if(global_setting.skip_sample(sample_data)):
+            continue
+        #SDU Hijack End
+
+        denoised = model(sample_data.x, sigmas[i] * s_in, **extra_args)
         sigma_down, sigma_up = get_ancestral_step(sigmas[i], sigmas[i + 1], eta=eta)
         if callback is not None:
-            callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigmas[i], 'denoised': denoised})
-        d = to_d(x, sigmas[i], denoised)
+            callback({'x': sample_data.x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigmas[i], 'denoised': denoised})
+        d = to_d(sample_data.x, sigmas[i], denoised)
         if sigma_down == 0:
             # Euler method
             dt = sigma_down - sigmas[i]
-            x = x + d * dt
+            sample_data.x = sample_data.x + d * dt
         else:
             # DPM-Solver-2
             sigma_mid = sigmas[i].log().lerp(sigma_down.log(), 0.5).exp()
             dt_1 = sigma_mid - sigmas[i]
             dt_2 = sigma_down - sigmas[i]
-            x_2 = x + d * dt_1
+            x_2 = sample_data.x + d * dt_1
             denoised_2 = model(x_2, sigma_mid * s_in, **extra_args)
             d_2 = to_d(x_2, sigma_mid, denoised_2)
-            x = x + d_2 * dt_2
-            x = x + noise_sampler(sigmas[i], sigmas[i + 1]) * s_noise * sigma_up
-    return x
+            sample_data.x = sample_data.x + d_2 * dt_2
+            sample_data.x = sample_data.x + noise_sampler(sigmas[i], sigmas[i + 1]) * s_noise * sigma_up
+    sample_end(sample_data)#SDU Hijack
+    return sample_data.x
 
